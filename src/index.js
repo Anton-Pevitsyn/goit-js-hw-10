270a4195b2662f2096e1a5db775815d72d648477
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-04-30 10:39:45 +0400

    error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/styles.css";
 import _ from "lodash";
 import API from "./js/fetchCounries";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -13,18 +14,46 @@ refs.countryNameInput.addEventListener("input", _.debounce(onSearch, 300));
 
 function onSearch(event) {
   const name = event.target.value;
-  console.log(API.fetchCountries(name));
-  API.fetchCountries(name).then(renderCounriList).catch();
+  API.fetchCountries(name).then(renderCounriList).catch(onError);
 }
 
 function renderCounriList(countries) {
-  console.log(countries);
-  if (countries.length > 1) {
+  // console.log(countries);
+  if (countries.length > 1 && countries.length < 6) {
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
     const markup = countries
       .map((element) => {
         return `<li class="item"><img src="${element.flags.png}" alt="flag"  width="30">${element.name.official}</li>`;
       })
       .join("");
     refs.countryList.insertAdjacentHTML("afterbegin", markup);
+  } else if (countries.length === 1) {
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
+    const markupDiv = `<h3 class="title-card"><img src="${
+      countries[0].flags.png
+    }" alt="flag" width="40" />${countries[0].name.official}</h3>
+<ul>
+  <li class="item-countri"><span class="argument">Capital: </span>${countries[0].capital.join(
+    ", "
+  )}</li>
+  <li class="item-countri"><span class="argument">Population: </span>${
+    countries[0].population
+  }</li>
+  <li class="item-countri"><span class="argument">Languges: </span>${Object.values(
+    countries[0].languages
+  ).join(", ")}</li>
+</ul>`;
+    refs.countryInfo.insertAdjacentHTML("afterbegin", markupDiv);
+  } else if (countries.length >= 6) {
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
+    Notify.info("Недостаточно данных");
   }
+}
+
+function onError() {
+  alert("dsfsdfsf");
+  Notify.warning("Нет такой страны");
 }
--- src/js/fetchCounries.js
+++ src/js/fetchCounries.js
@@ -1,8 +1,6 @@
 function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(
-    (response) => {
-      return response.json();
-    }
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then((response) =>
+    response.json()
   );
 }
 
